32a62e75389751c5d7e513e0b4b6592c6fa9d5e9
skgdrive932 <skgdrive932@gmail.com>
2026-08-19 18:35:04 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,43 +4,67 @@ plugins {
 }
 
 android {
+
     namespace = "com.example.volumepowerapp"
+
     compileSdk = 34
 
     defaultConfig {
+
         applicationId = "com.example.volumepowerapp"
+
         minSdk = 28
         targetSdk = 34
+
         versionCode = 1
         versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner =
+            "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
+
         release {
+
             isMinifyEnabled = false
+
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
+                getDefaultProguardFile(
+                    "proguard-android-optimize.txt"
+                ),
                 "proguard-rules.pro"
             )
         }
     }
 
     compileOptions {
+
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
+
         jvmTarget = "17"
     }
 }
 
 dependencies {
+
     implementation("androidx.core:core-ktx:1.12.0")
+
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.activity:activity-ktx:1.8.2")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    implementation(
+        "com.google.android.material:material:1.11.0"
+    )
+
+    implementation(
+        "androidx.activity:activity-ktx:1.8.2"
+    )
+
+    implementation(
+        "androidx.constraintlayout:constraintlayout:2.1.4"
+    )
 }
