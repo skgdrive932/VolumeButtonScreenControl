eebd5136714aa55f74aeff259fdc80e878a76881
skgdrive932 <skgdrive932@gmail.com>
2026-08-19 18:35:36 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,17 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    // Android Application plugin version
-    id("com.android.application") version "8.2.2" apply false
-    
-    // Kotlin Android plugin version
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+
+    id("com.android.application")
+        version "8.2.2"
+        apply false
+
+    id("org.jetbrains.kotlin.android")
+        version "1.9.22"
+        apply false
 }
 
-// Yeh task project clean karne ke liye hota hai
 tasks.register("clean", Delete::class) {
-    delete(rootProject.layout.buildDirectory)
+
+    delete(
+        rootProject.layout.buildDirectory
+    )
 }
